7d45287e4b89167a8cba22f7ae871fcfd0e98d5f
AshwiniKheersagar <ashwinikheersagar25032004@gmail.com>
2024-12-25 20:12:34 +0530

    Updates

---
+++ precache-manifest.73127ef2af31e776a3fa8c1d1e4a702a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "149ee13724d6a13988b4b1a9dadf5240",
+    "url": "/Job-Listing-website/index.html"
+  },
+  {
+    "revision": "ed5ec15eeb85219ec0e6",
+    "url": "/Job-Listing-website/static/css/2.b2154eb6.chunk.css"
+  },
+  {
+    "revision": "e9df8cf1bc0587ba1d67",
+    "url": "/Job-Listing-website/static/css/main.49ca8e89.chunk.css"
+  },
+  {
+    "revision": "ed5ec15eeb85219ec0e6",
+    "url": "/Job-Listing-website/static/js/2.2773a190.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/Job-Listing-website/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "e9df8cf1bc0587ba1d67",
+    "url": "/Job-Listing-website/static/js/main.98b702b6.chunk.js"
+  },
+  {
+    "revision": "074305c624f84ad20609",
+    "url": "/Job-Listing-website/static/js/runtime~main.31e4b767.js"
+  }
+]);
